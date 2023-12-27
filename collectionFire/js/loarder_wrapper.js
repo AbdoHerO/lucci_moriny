7c89f3e8d544d7eddbfd0649c3df647197f3e250
AbdoHerO <abdo.sbeta.12@gmail.com>
2023-12-27 01:34:26 +0100

    add landing page collexction fire

---
+++ collectionFire/js/loarder_wrapper.js
@@ -0,0 +1,24 @@
+window.addEventListener("load", function () {
+  $(".loader").hide();
+  $("body").removeClass("body_fixed");
+
+  // To track the ViewContent event using TikTok Pixel
+  ttq.track("ViewContent", {
+    contents: [
+      {
+        content_id: "1125",
+        content_name: "axi_inflatable_sofa",
+        quantity: 1,
+        price: 110,
+      },
+    ],
+    content_type: "product",
+    value: 110,
+    currency: "USD",
+  });
+});
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     var loaderWrapper = document.getElementById("loader-wrapper");
+//     loaderWrapper.style.display = "none";
+//   });
