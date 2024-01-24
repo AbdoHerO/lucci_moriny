6e6327584a5a278d4bf40e5d46fbae7f4d76535a
AbdoHerO <abdo.sbeta.12@gmail.com>
2024-01-24 20:27:50 +0100

    add casual-pant-decontracte lp

--- brosse_5_in_1/api_cozmo_landing_page.js
+++ brosse_5_in_1/api_cozmo_landing_page.js
@@ -20,7 +20,7 @@ $("#formInfo").submit(function (event) {
     city: "-",
     address: adresse,
     quantity: "1",
-    price: "249 Dh",
+    price: "199 Dh",
     product_notice: "",
     notice: "",
     status: "pending",
@@ -68,7 +68,7 @@ $("#formInfo").submit(function (event) {
         // // Display an error message if the update fails
         // alert("وقع حطأ اثناء الطلب , يرجى المحاولة لاحقا ");
 
-        //document.location.href = "/brosse_5_in_1/order_success.html";
+        document.location.href = "/brosse_5_in_1/order_success.html";
         // throw new Error("Failed to add order to SheetDB");
       }
     })
@@ -89,63 +89,63 @@ $("#formInfo").submit(function (event) {
 
 
   // Send an AJAX request to insert the order record
-  $.ajax({
-    url: "https://rachidcosm.dorimy.com/api/ordervisite",
-    type: "POST",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-    },
-    cors: true,
-    data: {
-      first_name: fullname,
-      last_name: "",
-      phone: phone,
-      city: "",
-      adresse: adresse,
-      id_product: "1132",
-      name_product: "Brosse 5 in 1",
-      unit_price: "249",
-      quantite: "1",
-      variant: "",
-      from_landing_page: true,
-    },
-    success: function (response) {
-      // // To track the purchase event using Facebook Pixel
-      // fbq("track", "Purchase", {
-      //   value: 10,
-      //   currency: "USD",
-      //   content_name: "axi_inflatable_sofa",
-      //   content_type: "home decoration",
-      //   product_id: "1132",
-      // });
+  // $.ajax({
+  //   url: "https://rachidcosm.dorimy.com/api/ordervisite",
+  //   type: "POST",
+  //   headers: {
+  //     "Access-Control-Allow-Origin": "*",
+  //   },
+  //   cors: true,
+  //   data: {
+  //     first_name: fullname,
+  //     last_name: "",
+  //     phone: phone,
+  //     city: "",
+  //     adresse: adresse,
+  //     id_product: "1132",
+  //     name_product: "Brosse 5 in 1",
+  //     unit_price: "249",
+  //     quantite: "1",
+  //     variant: "",
+  //     from_landing_page: true,
+  //   },
+  //   success: function (response) {
+  //     // // To track the purchase event using Facebook Pixel
+  //     // fbq("track", "Purchase", {
+  //     //   value: 10,
+  //     //   currency: "USD",
+  //     //   content_name: "axi_inflatable_sofa",
+  //     //   content_type: "home decoration",
+  //     //   product_id: "1132",
+  //     // });
 
-      document.location.href = "/brosse_5_in_1/order_success.html";
-      // hide loading icon and enable the button
-      //   $("#save_guest_order").prop("disabled", false);
-      //   $("#span_loading").hide();
-      console.log("response", response);
+  //     document.location.href = "/brosse_5_in_1/order_success.html";
+  //     // hide loading icon and enable the button
+  //     //   $("#save_guest_order").prop("disabled", false);
+  //     //   $("#span_loading").hide();
+  //     console.log("response", response);
 
-      // swal({
-      //   title: "تمت الطلبية بنجاح!",
-      //   text: "سيتصل بك فريقنا لتأكيد الطلبية",
-      //   icon: "success",
-      //   buttons: {
-      //     confirm: {
-      //       className: "btn btn-success",
-      //     },
-      //   },
-      // });
-    },
-    error: function (xhr, status, error) {
-      // hide loading icon and enable the button
-      //$("#save_guest_order").prop("disabled", false);
-      //$("#span_loading").hide();
-      //console.log("Error :", error);
-      // // Display an error message if the update fails
-      //alert("وقع حطأ اثناء الطلب , يرجى المحاولة لاحقا ");
+  //     // swal({
+  //     //   title: "تمت الطلبية بنجاح!",
+  //     //   text: "سيتصل بك فريقنا لتأكيد الطلبية",
+  //     //   icon: "success",
+  //     //   buttons: {
+  //     //     confirm: {
+  //     //       className: "btn btn-success",
+  //     //     },
+  //     //   },
+  //     // });
+  //   },
+  //   error: function (xhr, status, error) {
+  //     // hide loading icon and enable the button
+  //     //$("#save_guest_order").prop("disabled", false);
+  //     //$("#span_loading").hide();
+  //     //console.log("Error :", error);
+  //     // // Display an error message if the update fails
+  //     //alert("وقع حطأ اثناء الطلب , يرجى المحاولة لاحقا ");
 
-      document.location.href = "/brosse_5_in_1/order_success.html";
+  //     document.location.href = "/brosse_5_in_1/order_success.html";
 
-    },
-  });
+  //   },
+  // });
 });
