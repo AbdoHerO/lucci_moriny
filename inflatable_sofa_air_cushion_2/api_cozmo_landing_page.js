211c4c0bde04e1c4dc0e4f181ff3633077b17db3
AbdoHerO <abdo.sbeta.12@gmail.com>
2024-12-10 19:04:26 +0100

    add sofa

--- inflatable_sofa_air_cushion_2/api_cozmo_landing_page.js
+++ inflatable_sofa_air_cushion_2/api_cozmo_landing_page.js
@@ -27,7 +27,7 @@ $("#formInfo").submit(function (event) {
     city: "-",
     address: adresse,
     quantity: '1',
-    price: '999',
+    price: '1049',
     product_notice: "",
     notice: "-",
     status: "pending",
@@ -156,90 +156,3 @@ $("#formInfo").submit(function (event) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-$("#formInfo").submit(function (event) {
-  // show loading icon and disable the button
-  $("#save_guest_order").prop("disabled", true);
-  $("#span_loading").show();
-
-  // Prevent the default form submission
-  event.preventDefault();
-
-  // Get the updated data from the form
-  var fullname = $('#formInfo input[name="fullname"]').val();
-  var phone = $('#formInfo input[name="phone"]').val();
-  var adresse = $('#formInfo input[name="adresse"]').val();
-
-  // Create the data object for SheetDB
-  var sheetDBData = {
-    name: "Inflatable sofa",
-    date: new Date().toString(),
-    customer_name: fullname,
-    phone: phone,
-    city: "-",
-    address: adresse,
-    quantity: "1",
-    price: "999 Dh",
-    product_notice: "",
-    notice: "",
-    status: "pending",
-    fees_shipping: "",
-  };
-
-  // Insert into SheetDB API
-  fetch("https://sheetdb.io/api/v1/63vh1uqroaele", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ data: sheetDBData }),
-  })
-    .then(function (response) {
-      console.log("sent");
-      if (response.ok) {
-        // Handle successful response from SheetDB
-        console.log("Order added to SheetDB successfully");
-
-        // To track the purchase event using Facebook Pixel
-        fbq("track", "Purchase", {
-          value: 90,
-          currency: "USD",
-          content_name: "inflatable_sofa_air_cushion",
-          content_type: "home decoration",
-          product_id: "1063",
-        });
-
-        document.location.href = "/inflatable_sofa_air_cushion_2/order_success.html";
-
-        // To track the purchase event using Snap Pixel
-        // snaptr("track", "PURCHASE", { value: 132, currency: "USD" });
-      } else {
-        // Handle error response from SheetDB
-        console.log("Failed to add order to SheetDB");
-        $("#save_guest_order").prop("disabled", false);
-        $("#span_loading").hide();
-        console.log("Error :", error);
-        // throw new Error("Failed to add order to SheetDB");
-      }
-    })
-    .catch(function (error) {
-      console.log("NOT sent");
-      console.log("Error:", error);
-      // Display an error message if the request fails
-      // alert("Failed to add order to SheetDB. Please try again later.");
-    });
-
-  // Send an AJAX request to insert the order record
-
-});
